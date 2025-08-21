a80549175b9a14c97defbadf53c329093554253a
MohamedEmbrator <embrator233@gmail.com>
2025-08-22 01:19:38 +0300

    Explore

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Banner from "./components/Banner";
+import Explore from "./components/Explore";
 
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <Explore />
     </main>
   );
 }
